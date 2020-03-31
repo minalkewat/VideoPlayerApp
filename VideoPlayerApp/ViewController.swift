471a15eaabaf4346495712e81965cdbcf5b96ec7
minal <minalkewat@gmail.com>
2020-03-31 18:10:00 +0530

    Initial Commit

---
+++ VideoPlayerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayerApp
+//
+//  Created by apple on 31/03/20.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

051c418a1e70d5ae1095f6b9f0725bb3190b5627
minal <minalkewat@gmail.com>
2020-03-31 18:27:06 +0530

    A Video Player App

--- VideoPlayerApp/ViewController.swift
+++ VideoPlayerApp/ViewController.swift
@@ -7,9 +7,24 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
+    @IBAction func tapOnWatchVideo(_ sender: UIButton) {
+        
+        if let path = Bundle.main.path(forResource: "video", ofType: "mp4"){
+            
+            let video = AVPlayer(url: URL(fileURLWithPath: path))
+            let videoPlayer =  AVPlayerViewController()
+            videoPlayer.player = video
+            
+            present(videoPlayer, animated: true) {
+                video.play()
+            }
+            
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
